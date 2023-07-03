9e0e375f43b467ea76bb09166f86076a5ff704d6
Lawrence <sylandia1@gmail.com>
2023-07-03 19:33:27 -0400

    Files for Firbase. - New branch to fix dependency duplicate issue for some reason. - Login and Register page created and styled. --> To do java code for pages.

---
+++ app/src/main/java/com/example/spotter/View/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.spotter.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.spotter.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

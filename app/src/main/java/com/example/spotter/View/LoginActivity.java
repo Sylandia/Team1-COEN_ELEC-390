2c5c8b5bf4cce628ebb8f7e3a66556ca7945a3ed
Lawrence <sylandia1@gmail.com>
2023-07-05 19:12:17 -0400

    - Sign in and Sign up connected - Sign out temporarily LoginActivity - FirebaseHelper created for cleaning after functionality works
    -Todo: clean out commented codes, connect with activity page after login, and functional sign out

--- app/src/main/java/com/example/spotter/View/LoginActivity.java
+++ app/src/main/java/com/example/spotter/View/LoginActivity.java
@@ -2,15 +2,50 @@ package com.example.spotter.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.spotter.Controller.FirebaseHelper;
 import com.example.spotter.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-
+    static final String Lobster = "Lobster_Log";
+    Button signout;
+    private FirebaseUser user;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        signout = findViewById(R.id.signOutButton);
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+
+        if(user == null){
+            goToLogin();
+            finish();
+        }
+
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(Lobster, "Logged out.");
+                FirebaseAuth.getInstance().signOut();
+                goToLogin();
+                finish();
+            }
+        });
+
+    }
+    private void goToLogin(){
+
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+
     }
 }

57c19190c23590eba6e07b8714b6150cab0a6bb0
Salem <salloum01@hotmail.com>
2023-07-05 22:08:07 -0400

    Added some stuff

--- app/src/main/java/com/example/spotter/View/MainActivity.java
+++ app/src/main/java/com/example/spotter/View/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.spotter.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,5 +18,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernameText = findViewById(R.id.usernameText);
+        passwordText = findViewById(R.id.passwordText);
+        logInButton = findViewById(R.id.logInButton);
+        signInButton = findViewById(R.id.signInButton);
+
+        signInButton.setOnClickListener(signInActivity);
+        logInButton.setOnClickListener(logInActivity);
     }
+
+    private View.OnClickListener signInActivity = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getBaseContext(), SignInActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    private View.OnClickListener logInActivity = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getBaseContext(), HomeActivity.class);
+            startActivity(intent);
+        }
+    };
 }
--- app/src/main/java/com/example/spotter/View/SignInActivity.java
+++ app/src/main/java/com/example/spotter/View/SignInActivity.java
@@ -1,6 +1,7 @@
 package com.example.spotter.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.widget.Button;
@@ -8,13 +9,18 @@ import android.widget.EditText;
 
 import com.example.spotter.R;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
     private EditText usernameText, passwordText, confirmPasswordText;
     private Button confirmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_sign_in);
+
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
